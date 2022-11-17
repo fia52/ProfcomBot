e37306d8de278c6df361dfc2dbb13cc4c9af79aa
fia52 <froloviliya.best@yandex.ru>
2022-11-17 15:46:44 +0300

    func start_bot added

--- bot.py
+++ bot.py
@@ -8,9 +8,13 @@ async def on_startup(_):
     print('Bot is now online')
     sql_start()
 
-admin.register_handlers_admin(dp)
-client.register_handlers_client(dp)
 
-other.register_handlers_other(dp)  # порядок регистрации хендлеров важен
+def start_bot():
+    admin.register_handlers_admin(dp)
+    client.register_handlers_client(dp)
+    other.register_handlers_other(dp)
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup)  # 'skip_updates=True' - бот не будет отвечать на сообщения, которые пришли, пока он не был активен
 
-executor.start_polling(dp, skip_updates=True, on_startup=on_startup)  # 'skip_updates=True' - бот не будет отвечать на сообщения, которые пришли, пока он не был активен
+
+if __name__ == '__main__':
+    start_bot()
